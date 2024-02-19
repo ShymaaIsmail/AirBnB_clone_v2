79b1b265785bd0a9a0c2dc188e42f3544d94d1fd
3zzazakl <vanzekovichgr@gmail.com>
2024-02-19 23:53:56 +0200

    checking task6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.city import City
 
 
-class State(BaseModel):
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+
+        cities = relationship('City', cascade="all, delete", backref="state")
+
+    else:
+        name = ""
+        @property
+        def cities(self):
+            """Getters"""
+            from models import storage
+            citiesLst = []
+            allCities = storage.all(City)
+            for city in allCities.values():
+                if city.state_id == self.id:
+                    citiesLst.append(city)
+            return (citiesLst)
