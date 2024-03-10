68795625abf7f8b1cb436e76c80356dadc4bb761
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-10 12:49:51 +0300

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Packing the web static"""
+from fabric import task
+from fabric.operations import local
+from datetime import datetime
+import os
+
+@task
+def do_pack(c):
+    """Packs content of web static folder into tgz archive"""
+    archive_path = None
+    dest_folder = "versions"
+    now = datetime.now()
+    time_stamp = f"{now.year}{now.month}{now.day}{now.hour} \
+                    {now.minute}{now.second}"
+    tar_file_name = f"web_static_{time_stamp}.tgz"
+    archive_path = os.path.join(dest_folder, tar_file_name)
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
+    try:
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except Exception as e:
+        return None
