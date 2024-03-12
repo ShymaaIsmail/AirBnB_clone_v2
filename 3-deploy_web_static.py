eff0e17f62f728afdd6ab2a8d65bce277098215f
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-12 10:38:53 +0300

    task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Full Deployment includes pack and deployment"""
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+
+def deploy():
+    """Full Deployment calling do_pack and do_deploy"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    else:
+        return do_deploy(archive_path)
