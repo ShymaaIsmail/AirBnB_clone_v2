bd87259369f2b9d0c7bc0711592bb97a6059946e
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2024-03-12 14:51:49 +0300

    Update 3-deploy_web_static.py (#16)

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,90 +1,41 @@
 #!/usr/bin/python3
-"""
-Fabric script that generates a .tgz archive from the contents of the web_static
-folder and distributes it to web servers.
-"""
-import os
-from datetime import datetime
+"""Full Deployment includes pack and deployment"""
+from fabric.api import env, task, local
+from os.path import exists
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+archive_path = do_pack()
 
-from fabric.api import env, local, put, sudo, task
-from fabric.decorators import runs_once
 
-# List of servers to deploy to
 env.hosts = ['54.160.107.3', '52.3.245.179']
 env.user = 'ubuntu'
 
 
-@runs_once
-def do_pack():
-    """
-    Generates a .tgz archive from the contents of the web_static folder.
-    The archive is stored in the 'versions' folder.
-    """
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_name = f"web_static_{timestamp}.tgz"
-    archive_path = os.path.join("versions", archive_name)
-
-    # Create 'versions' directory if it doesn't exist
-    os.makedirs("versions", exist_ok=True)
-
-    # Create a .tgz archive of the web_static directory
-    print(f"Packing web_static to {archive_path}")
-    result = local(f"tar -cvzf {archive_path} web_static", capture=False)
-
-    if result.return_code == 0:
-        size = os.path.getsize(archive_path)
-        print(f"web_static packed: {archive_path} -> {size}Bytes")
-        return archive_path
-
-    return None
-
-
-def do_deploy(archive_path):
-    """
-    Distributes an archive to your web servers
-    """
-    if not os.path.exists(archive_path):
+def local_deploy(archive_path):
+    """Connect to local host and push the archive file"""
+    if not exists(archive_path):
         return False
-
-    try:
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, "/tmp/")
-        archive_name = os.path.basename(archive_path)
-        archive_name_no_ext = archive_name.split(".")[0]
-        release_dir = f"/data/web_static/releases/{archive_name_no_ext}"
-
-        # Uncompress the archive to the folder /data/web_static/releases/
-        sudo(f"mkdir -p {release_dir}")
-        sudo(f"tar -xzf /tmp/{archive_name} -C {release_dir}")
-
-        # Copy the contents of web_static to the parent directory
-        sudo(f"rsync -a {release_dir}/web_static/ {release_dir}/")
-
-        # Remove the now empty web_static directory
-        sudo(f"rm -rf {release_dir}/web_static")
-
-        # Delete the archive from the web server
-        sudo(f"rm -rf /tmp/{archive_name}")
-
-        # Delete the symbolic link /data/web_static/current from the web server
-        sudo("rm -rf /data/web_static/current")
-
-        # Create a new the symbolic link /data/web_static/current on server
-        # linked to the new version of your code
-        sudo(f"ln -s {release_dir} /data/web_static/current")
-
-        print("New version deployed!")
-        return True
-    except Exception:
-        return False
+    else:
+        try:
+            file_n = archive_path.split("/")[-1]
+            no_ext = file_n.split(".")[0]
+            path = "/data/web_static/releases/"
+            # For localhost, use local() instead of put()
+            local('mkdir -p {}{}/'.format(path, no_ext))
+            local('tar -xzf {} -C {}{}/'.format(archive_path, path, no_ext))
+            local('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+            local('rm -rf {}{}/web_static'.format(path, no_ext))
+            local('rm -rf /data/web_static/current')
+            local('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+            return True
+        except Exception as e:
+            raise e
 
 
-@task(default=True)
+@task
 def deploy():
-    """
-    Creates and distributes an archive to your web servers
-    """
-    archive_path = do_pack()
+    """Full Deployment calling do_pack and do_deploy"""
     if archive_path is None:
         return False
-    return do_deploy(archive_path)
+    else:
+        return do_deploy(archive_path)
