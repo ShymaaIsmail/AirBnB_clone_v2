fce6bb23c87d4719bba8b3c63d42157751e0f810
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-12 11:47:04 +0300

    fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,70 +1,17 @@
 #!/usr/bin/python3
-"""
-Fabric script that creates and distributes an archive to multiple web servers
-"""
+"""Full Deployment includes pack and deployment"""
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+from fabric.api import env
+archive_path = do_pack()
 
-from fabric.api import env, local, put, run
-from datetime import datetime
-from os.path import exists, isdir
 
-# Define the list of hosts
 env.hosts = ['54.160.107.3', '52.3.245.179']
 env.user = 'ubuntu'
 
-def do_pack():
-    """Generates a tgz archive"""
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except Exception as e:
-        print("Error occurred during packing:", e)
-        return None
-
-def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
-    if exists(archive_path) is False:
-        print("Archive does not exist:", archive_path)
-        return False
-    
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except Exception as e:
-        print("Error occurred during deployment:", e)
-        return False
-
 def deploy():
-    """Creates and distributes an archive to the web servers"""
-    archive_path = do_pack()
+    """Full Deployment calling do_pack and do_deploy"""
     if archive_path is None:
-        print("Failed to create archive")
         return False
-    
-    for host in env.hosts:
-        env.host_string = host
-        print(f"Deploying to {host}")
-        success = do_deploy(archive_path)
-        if not success:
-            print(f"Failed to deploy to {host}")
-            return False
-    
-    print("Deployment successful")
-    return True
-
-# Call deploy function when the script is executed directly
-if __name__ == "__main__":
-    deploy()
+    else:
+        return do_deploy(archive_path)
