8e1d614472106db282fbb973ef62e3ce417ed74e
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-12 11:51:17 +0300

    full

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,13 +2,14 @@
 """Full Deployment includes pack and deployment"""
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
-from fabric.api import env
+from fabric.api import env, task
 archive_path = do_pack()
 
 
 env.hosts = ['54.160.107.3', '52.3.245.179']
 env.user = 'ubuntu'
 
+@task
 def deploy():
     """Full Deployment calling do_pack and do_deploy"""
     if archive_path is None:
