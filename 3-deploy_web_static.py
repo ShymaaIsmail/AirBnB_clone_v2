039ad967ced46048dda6e7ad76f066beaeba0493
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-12 11:52:25 +0300

    stye

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Full Deployment includes pack and deployment"""
+from fabric.api import env, task
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
-from fabric.api import env, task
 archive_path = do_pack()
 
 
 env.hosts = ['54.160.107.3', '52.3.245.179']
 env.user = 'ubuntu'
+
 
 @task
 def deploy():
