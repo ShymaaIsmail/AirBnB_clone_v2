ac09b0a7b33d797c988e9c28a48d7fcd7f512cb8
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-25 01:35:39 +0300

    9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-""" Hello Route as a Basic usage for flask routing"""
+"""
+Script that starts a Flask web application
+"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """List All States"""
-    states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
-
-
-
 @app.teardown_appcontext
-def close_app(exception):
-    """close session"""
+def teardown(exception):
+    """After each request, remove the current SQLAlchemy Session"""
     storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with a list of all State objects and their cities"""
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == "__main__":
