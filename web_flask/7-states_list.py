5357752ac77bfb651015b63bf7fc658b1519739d
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-25 01:10:48 +0300

    fded

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,53 +7,10 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Hello HBNB Message"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb_only():
-    """HBNB Message"""
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c_text(text=None):
-    """C Text parameter"""
-    return f"C {str.replace(text, '_', ' ')}"
-
-
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python_text(text="is cool"):
-    """python Text parameter"""
-    return f"Python {str.replace(text, '_', ' ')}"
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def number_only(n):
-    """number parameter only"""
-    return f"{n} is a number"
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def html_number_only(n):
-    """number template only"""
-    return render_template("5-number.html", n=n)
-
-
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def html_number_odd_or_even(n):
-    """number odd or even template only"""
-    return render_template("6-number_odd_or_even.html", n=n)
-
-
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """List All States"""
-    states = storage.all(State).values()
+    states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 
