94a2788c5d299ecbfb19c6ba38023742665314b0
shymaaismail <shymaa.m.ismail@gmail.com>
2024-03-25 02:21:12 +0300

    tak 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,6 +4,7 @@
 
 from flask import Flask, render_template
 from models import storage
+from models.city import City
 from models.state import State
 app = Flask(__name__)
 
@@ -14,12 +15,24 @@ def teardown_db(exception=None):
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route("/states", strict_slashes=False)
 def states_list():
     """List All States"""
     states = storage.all(State).values()
     states = sorted(states, key=lambda state: state.name)
     return render_template("7-states_list.html", states=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_by_id(id=None):
+    """List state by id"""
+    states = storage.all(State)
+    state = next(filter(lambda state: state.id == id, states.values()), None)
+    cities = None
+    if id:
+        all_cities = storage.all(City)
+        cities = [city for city in all_cities.values() if city.state_id == id]
+    return render_template('9-states.html', state=state, cities=cities)
 
 
 if __name__ == "__main__":
